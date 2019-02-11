f3cb4a08f0fe088076df9fa2c7282414163b1a01
Shiva <Shiva@Shiva-PC>
2019-02-11 21:08:05 +0530

    Commit Servlet is Modified

--- src/com/pack/TestServlet.java
+++ src/com/pack/TestServlet.java
@@ -27,6 +27,7 @@ public class TestServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		System.out.println("Servlet modified in branch");
 	}
 
 }
